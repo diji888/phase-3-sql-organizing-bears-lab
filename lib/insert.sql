00d93a166db00860558e49786da2334a8894c503
Diana Jiang <jiang99diana@gmail.com>
2021-12-18 19:40:54 -0500

    commit tree 8361341845e7e0448857c9087f1b4c6a80ab5fdf

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ("Mr.Chocolate", 7, "M", "chocolate brown", "chill", 1),
+    ("Rowdy", 3, "M", "cherry brown", "not chill", 1),
+    ("Tabitha", 4, "F", "pale brown", "not chill", 1),
+    ("Sergeant Brown", 15, "M", "camo brown", "not chill", 0),
+    ("Melissa", 1, "F", "dark brown", "chill", 1),
+    ("Grinch", 9, "M", "greenish brown", "not chill", 1),
+    ("Wendy", 12, "F", "gray brown", "chill", 0),
+    (NULL, 2, "M", "dark brown", "chill", 1);
